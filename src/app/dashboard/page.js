dd4befa214e34d4ba5987a1e45444c49ce7512f7
samchl <happysam31640@gmail.com>
2024-05-09 01:34:02 +0800

    Modify dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,6 +4,16 @@ import { useEffect } from 'react';
 import { useLiff } from '../_components/liff-provider';
 
 export default function DashboardPage() {
-  const { liffObject, liffError } = useLiff();
+  const { liffObject } = useLiff();
+
+  useEffect(() => {
+    if (!liffObject) return;
+
+    const idToken = liffObject.getIDToken();
+    fetch('/api/auth', {
+      method: 'POST',
+      body: JSON.stringify({ idToken }),
+    });
+  }, [liffObject]);
   return <div>Dashboard Page</div>;
 }
