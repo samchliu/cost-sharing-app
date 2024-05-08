9e59c3a3e70e67113e7e76a775ea2cba64483d13
Ruby <rruby6120tw@gmail.com>
2024-05-08 20:35:04 +0800

    feat: modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ['inter', 'sans-serif'],
       },
+      colors: {
+        dark: '#5F5D5E',
+        light: '#FFFFFF',
+        primary: '#FAFC2C',
+        primaryDark: '#1C1A1B',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    screens: {
+      '3xl': { max: '2200px' },
+      // => @media (max-width: 1535px) { ... }
+
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: '479px' },
+      // => @media (max-width: 479px) { ... }
     },
   },
   plugins: [],
