b78ef9fcafe2caf16db0e1f92296391991462801
Ruby <rruby6120tw@gmail.com>
2024-05-23 21:20:34 +0800

    feat: modified add group button

--- src/app/(routes)/groups/page.tsx
+++ src/app/(routes)/groups/page.tsx
@@ -1,25 +1,27 @@
 'use client';
 // import { useEffect, useState } from 'react';
 // import { useLiff } from '@/app/_components/liff-provider';
-// import Image from 'next/image';
+import Image from 'next/image';
 import GroupButton from '@/app/ui/groups/GroupButton';
 import { groups } from '@/app/_components/dummyData';
-
+import addGroupIcon from '../../../../public/icons/addGroup.svg';
 
 export default function Page() {
  const data = groups;
 
  return (
    <div className="flex flex-col">
-     <h1 className="fixed left-[50%] z-[2] mt-0 w-full translate-x-[-50%] bg-primary-100 py-7 text-center text-3xl">
-       Tool Name
+     <h1 className="fixed left-[50%] z-[2] mt-0 w-full translate-x-[-50%] bg-primary-100 py-7 text-center text-3xl font-normal">
+       Chill 後算賬
      </h1>
      <div className="mt-20">
        {data ? data.map((group) => <GroupButton key={group.groupId} group={group} />) : null}
      </div>
+     <div className="h-24"></div>
      <div className="fixed bottom-0 left-[50%] z-[2] flex h-24 w-full translate-x-[-50%] items-center justify-center bg-primary-100">
-       <button className="w-[92%] rounded-[10px] bg-primary-200 p-[13px] active:bg-grey-200">
-         Add Group
+       <button className="flex w-[92%] items-center justify-center rounded-[10px] bg-primary-200 p-[13px] active:bg-grey-200">
+         <Image src={addGroupIcon} alt="add group icon" />
+         <span className="pl-2">新增群組</span>
        </button>
      </div>
    </div>
