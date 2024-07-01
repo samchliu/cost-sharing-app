72bf9265e310fe3880a1c9dec20160d483f9e010
samchl <happysam31640@gmail.com>
2024-07-02 00:15:44 +0800

    feat: add get user data route

---
+++ src/app/api/(protected)/user/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse, NextRequest } from 'next/server';
+import prisma from '@/lib/db';
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const id = params.id;
+    const user = await prisma.user.findUnique({
+      where: { id },
+      include: {
+        groupUsers: {
+          select: {
+            group: {
+              select: {
+                id: true,
+                name: true,
+                picture: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!user) return new NextResponse('No such user exists', { status: 404 });
+    const responseBody: any = { ...user, groups: user.groupUsers.map((item) => item.group) };
+    delete responseBody.groupUsers;
+    return NextResponse.json(responseBody);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
