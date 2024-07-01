6633d753342b08c6c04a468aad3d2c5fae7c6de4
samchl <happysam31640@gmail.com>
2024-07-02 00:27:20 +0800

    feat: modify delete group route

--- src/app/api/(protected)/group/[groupId]/route.ts
+++ src/app/api/(protected)/group/[groupId]/route.ts
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 
 export async function DELETE(request: NextRequest, { params }: { params: { groupId: string } }) {
-    try {
-        const group = await prisma.group.findUnique({ where: { id: params.groupId } });
-        if (!group) return new NextResponse('Group does not exist', { status: 404 });
-        
-        await prisma.group.delete({ where: { id: params.groupId } });
-        return new NextResponse(undefined, { status: 204 });
-      } catch (error) {
-        console.error(error);
-        return NextResponse.error();
-      }
+  try {
+    const group = await prisma.group.findUnique({ where: { id: params.groupId } });
+    if (!group) return new NextResponse('Group does not exist', { status: 404 });
+
+    await prisma.group.delete({ where: { id: params.groupId } });
+    return new NextResponse(undefined, { status: 204 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
