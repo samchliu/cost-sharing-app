6bb11ca4c46d414c4a6d11dfbac0f78785b13390
samchl <happysam31640@gmail.com>
2024-07-02 00:27:39 +0800

    feat: add get group date route

--- src/app/api/(protected)/group/[groupId]/route.ts
+++ src/app/api/(protected)/group/[groupId]/route.ts
@@ -1,6 +1,45 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 
+export async function GET(request: NextRequest, { params }: { params: { groupId: string } }) {
+  try {
+    const group = await prisma.group.findUnique({
+      where: { id: params.groupId },
+      include: {
+        groupUsers: {
+          select: {
+            user: {
+              select: {
+                id: true,
+                name: true,
+                picture: true,
+              },
+            },
+          },
+        },
+        expenses: {
+          select: {
+            id: true,
+            name: true,
+            amount: true,
+            date: true,
+            note: true,
+            payerId: true,
+          },
+        },
+      },
+    });
+    if (!group) return new NextResponse('Group does not exist', { status: 404 });
+
+    const responseBody: any = { ...group, users: group.groupUsers.map((item) => item.user) };
+    delete responseBody.groupUsers;
+    return NextResponse.json(responseBody);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
+
 export async function DELETE(request: NextRequest, { params }: { params: { groupId: string } }) {
   try {
     const group = await prisma.group.findUnique({ where: { id: params.groupId } });
